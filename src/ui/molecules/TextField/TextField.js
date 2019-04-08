737e19a0a446643b6b22e230d6db825082e1c7e1
Анастасия Сёмина <na@appkode.ru>
2019-04-08 20:22:24 +0200

    fixes

--- src/ui/molecules/TextField/TextField.js
+++ src/ui/molecules/TextField/TextField.js
@@ -11,8 +11,7 @@ const TextFieldContainer = styled.div`
   display: flex;
   flex-direction: row;
   border-radius: 4px;
-  /* background-color: ${({ theme }) => theme.pallete.darkWhite}; */
-  background-color: #eee;
+  background-color: ${({ theme }) => theme.pallete.darkWhite};
   height: 40px;
   min-height: 40px;
   border: 1px
