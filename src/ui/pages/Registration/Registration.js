4d1037341a5a958db241addf53a44aa96c312bdf
sirvadim <sir.vadim2013@yandex.ru>
2019-04-11 02:10:21 +0200

    добавил обертку и плейсхолдеры

--- src/ui/pages/Registration/Registration.js
+++ src/ui/pages/Registration/Registration.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react'
-import { PageTemplate, HBox, Flex1, Divider } from '@ui/atoms'
+import { PageTemplate, HBox, VBox, Flex1, Divider } from '@ui/atoms'
 import { styled } from '@ui/theme'
 import { Body2 } from '@ui/atoms/Typography'
 import {
@@ -14,6 +14,10 @@ import {
 import { action } from '@storybook/addon-actions'
 import { Formik, Field } from 'formik'
 import * as Yup from 'yup'
+
+const Wrapper = styled.div`
+  padding: ${({ theme }) => theme.paddings.main}px;
+`
 
 // todo onError checkbox
 const schema = Yup.object().shape({
@@ -70,86 +74,93 @@ const mockData = {
 export const Registration = () => {
   return (
     <PageTemplate>
-      <Header title="Введите код" />
-      <Formik
-        validationSchema={schema}
-        initialValues={mockData}
-        onSubmit={values => console.log('values', values)}
-        render={props => (
-          <form>
-            <TextField
-              name="name"
-              label="Ваше имя"
-              value={props.values.name}
-              onChange={value => props.setFieldValue('name', value)}
-              onBlur={props.handleBlur}
-              error={props.touched.name && props.errors.name}
-            />
-            <TextField
-              name="surname"
-              label="Ваша фамилия"
-              value={props.values.surname}
-              onChange={value => props.setFieldValue('surname', value)}
-              onBlur={props.handleBlur}
-              error={props.touched.surname && props.errors.surname}
-            />
-            <TextField
-              name="phone"
-              label="Номер телефона"
-              startAdornment="+7"
-              placeholder="9XXXXXXXXX"
-              value={props.values.phone}
-              onChange={value => props.setFieldValue('phone', value)}
-              onBlur={props.handleBlur}
-              error={props.touched.phone && props.errors.phone}
-            />
-            <TextField
-              name="mail"
-              label="Email"
-              value={props.values.mail}
-              onChange={value => props.setFieldValue('mail', value)}
-              onBlur={props.handleBlur}
-              error={props.touched.mail && props.errors.mail}
-            />
-            {/* Пол */}
-            <TextareaField
-              value={props.values.about}
-              label="Немного о себе"
-              placeholder="Ваше хобби, любимые книги и т.д."
-              onChange={value => props.setFieldValue('about', value)}
-              onBlur={props.handleBlur}
-              error={props.touched.about && props.errors.about}
-            />
-            <CheckboxField
-              // component="checkbox"
-              name="terms"
-              // type="checkbox"
-              // component={CheckboxField}
-              // checked={props.values.terms}
-              onPress={() => {
-                console.log(props)
-                props.values.terms = !props.values.terms
-                props.handleChange('terms')
-                // props.isValid = true
-                // props.submitForm()
-                // props.handleSubmit()
-                // console.log('valid?', props.isValid)
-              }}
-            />
-            {/* <Field
+      <Header title="Регистрация" />
+      <Flex1>
+        <Wrapper>
+          <Formik
+            validationSchema={schema}
+            initialValues={mockData}
+            onSubmit={values => console.log('values', values)}
+            render={props => (
+              <form>
+                <TextField
+                  name="name"
+                  label="Ваше имя"
+                  placeholder="Иван"
+                  value={props.values.name}
+                  onChange={value => props.setFieldValue('name', value)}
+                  onBlur={props.handleBlur}
+                  error={props.touched.name && props.errors.name}
+                />
+                <TextField
+                  name="surname"
+                  label="Ваша фамилия"
+                  placeholder="Иванов"
+                  value={props.values.surname}
+                  onChange={value => props.setFieldValue('surname', value)}
+                  onBlur={props.handleBlur}
+                  error={props.touched.surname && props.errors.surname}
+                />
+                <TextField
+                  name="phone"
+                  label="Номер телефона"
+                  startAdornment="+7"
+                  placeholder="XXXXXXXXXX"
+                  value={props.values.phone}
+                  onChange={value => props.setFieldValue('phone', value)}
+                  onBlur={props.handleBlur}
+                  error={props.touched.phone && props.errors.phone}
+                />
+                <TextField
+                  name="mail"
+                  label="Email"
+                  placeholder="simple@mail.com"
+                  value={props.values.mail}
+                  onChange={value => props.setFieldValue('mail', value)}
+                  onBlur={props.handleBlur}
+                  error={props.touched.mail && props.errors.mail}
+                />
+                {/* Пол */}
+                <TextareaField
+                  name="about" // warning wtf??? todo
+                  value={props.values.about}
+                  label="Немного о себе"
+                  placeholder="Ваше хобби, любимые книги и т.д."
+                  onChange={value => props.setFieldValue('about', value)}
+                  onBlur={props.handleBlur}
+                  error={props.touched.about && props.errors.about}
+                />
+                <CheckboxField
+                  // component="checkbox"
+                  name="terms"
+                  // type="checkbox"
+                  // component={CheckboxField}
+                  // checked={props.values.terms}
+                  onPress={() => {
+                    console.log(props)
+                    props.values.terms = !props.values.terms
+                    props.handleChange('terms')
+                    // props.isValid = true
+                    // props.submitForm()
+                    // props.handleSubmit()
+                    // console.log('valid?', props.isValid)
+                  }}
+                />
+                {/* <Field
               type="checkbox"
               name="terms"
             /> */}
-            {/* <HBox /> */}
-            <ButtonAccent
-              disabled={!props.isValid}
-              onPress={props.handleSubmit}
-            >
-              Send
-            </ButtonAccent>
-          </form>
-        )}
-      />
+                <ButtonAccent
+                  disabled={!props.isValid}
+                  onPress={props.handleSubmit}
+                >
+                  Send
+                </ButtonAccent>
+              </form>
+            )}
+          />
+        </Wrapper>
+      </Flex1>
     </PageTemplate>
   )
 }
